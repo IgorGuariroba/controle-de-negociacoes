9dc718fb5ace23288c3bede8e93ed49a99f5d428
IgorGuariroba <1g0r.guari@gmail.com>
2022-08-17 09:08:23 -0300

    Lançar um erro caso o testo da data não seja entregue no formato correto

--- app/helpers/DateHelper.js
+++ app/helpers/DateHelper.js
@@ -5,10 +5,15 @@ class DateHelper {
     }
 
     static textoParaData(texto) {
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2))
+
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)){
+            throw new Error('Deve estar no formato aaaa-mm-dd')
+        }
+
+        return new Date(...texto.split(`-`).map((item, indice) => item - indice % 2))
     }
 
-   static dataParaFormatStringBr(data) {
-        return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear()
+    static dataParaFormatStringBr(data) {
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`
     }
 }
